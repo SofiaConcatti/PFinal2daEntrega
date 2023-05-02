31d07a0f068d90369552debd4bef1de5f3b0a1bf
Sofia Concatti <concattisofia@gmail.com>
2023-05-02 19:09:04 -0300

    Segunda entrega Proyecto Final

---
+++ app/shared/components/custom-spinner/custom-spinner.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-spinner',
+  templateUrl: './custom-spinner.component.html',
+  styleUrls: ['./custom-spinner.component.scss'],
+})
+export class CustomSpinnerComponent {}
---
+++ app/shared/shared.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ErrorHelperComponent } from './components/error-helper/error-helper.component';
+import { ThemeSwitcherComponent } from './components/theme-switcher/theme-switcher/theme-switcher.component';
+import { MyMaterialModule } from './modules/my-material.module';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { CustomSpinnerComponent } from './components/custom-spinner/custom-spinner.component';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+@NgModule({
+  declarations: [
+    ErrorHelperComponent,
+    ThemeSwitcherComponent,
+    NotFoundComponent,
+    CustomSpinnerComponent,
+  ],
+  imports: [
+    CommonModule,
+    MyMaterialModule,
+    ReactiveFormsModule,
+    FormsModule,
+    RouterModule,
+    MatSlideToggleModule,
+  ],
+  exports: [ErrorHelperComponent, ThemeSwitcherComponent],
+})
+export class SharedModule {}
